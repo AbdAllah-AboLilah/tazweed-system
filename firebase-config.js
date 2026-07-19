5d3c0ae74489de8d649d14145356fd32fc5acaf6
AbdAllah-AboLilah <dodododo054@gmail.com>
2026-07-19 12:16:13 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -6,12 +6,12 @@
 // في المستودع حتى لو Private.
 
 const firebaseConfig = {
-  apiKey: "ضع_apiKey_هنا",
-  authDomain: "ضع_authDomain_هنا",
-  projectId: "ضع_projectId_هنا",
-  storageBucket: "ضع_storageBucket_هنا",
-  messagingSenderId: "ضع_messagingSenderId_هنا",
-  appId: "ضع_appId_هنا",
+  apiKey: "AIzaSyA5Oz3t9ba5--wkoxR_aPyfAsLeCbha8X8",
+  authDomain: "tazweed-system.firebaseapp.com",
+  projectId: "tazweed-system",
+  storageBucket: "tazweed-system.firebasestorage.app",
+  messagingSenderId: "558083736456",
+  appId: "1:558083736456:web:51aa9c9309bfb33527703d",
 };
 
 // لو لسه محطتش القيم الحقيقية، النظام هيوريك رسالة واضحة بدل شاشة بيضة.
--- js/app-info.js
+++ js/app-info.js
@@ -2,4 +2,4 @@
 // عند كل تحديث فعلي، ارفع رقم الإصدار هنا.
 
 const APP_NAME = 'نظام التزويد';
-const APP_VERSION = '0.3.1';
+const APP_VERSION = '0.3.2';
